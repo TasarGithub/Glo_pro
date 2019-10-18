5c17daa8be7c06e20842fd6cfc75b0bcc3c42cd7
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-10-18 13:43:16 +0300

    Исправлена ошибка в src

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
-let money;
-let income; 
-let addExpenses;
-let deposit;
-let mission; 
-let period;
-alert( "Ready, Steady, Go" );
-console.log( "Execution begin" );
+let money = 40000; // Доход за месяц
+let income = "40000"; //Доп доход, фриланс
+let addExpenses = "Кредит,Ремонт,Обучение"; // Доп расходы 
+let deposit = true; // Наличие депозита
+let mission = 1500000; // Какую сумму хочу накопить
+let period = 12; //Период
+
+console.log( typeof(money) );
+console.log( typeof(income) );
+console.log( typeof(deposit) );
