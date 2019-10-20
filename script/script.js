687cd570476d3df0534a7f0450a427d7a60a3161
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-10-20 15:47:56 +0300

    edit2 lessson1

--- script/script.js
+++ script/script.js
@@ -6,17 +6,12 @@ let mission = 1500000; // Какую сумму хочу накопить
 let period = 12; //Период
 let budgetDay = money/30; //Дневной бюджет
 
-console.log( "typeof(money)" );
-console.log( typeof(money) );
-console.log( "income.length" );
-console.log( income.length );
-console.log( "typeof(deposit)");
-console.log( typeof(deposit) );
+console.log( "typeof(money): "+ typeof(money) );
+console.log( "income.length: "+ income.length );
+console.log( "typeof(deposit): "+ typeof(deposit));
 console.log( "Период " + period + " месяцев. " + 
 "Цель заработать "+ mission +" рублей" );
 addExpenses = addExpenses.toLowerCase();
-console.log("budgetDay");
-console.log( budgetDay);
-console.log( "money % 30" );
-console.log( money % 30 );
-
+console.log("addExpenses as array: " + addExpenses.split("") );
+console.log( "budgetDay: " + budgetDay);
+console.log( "money % 30: " + money % 30 );
