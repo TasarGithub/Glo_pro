a0b3755ee98ff09c3c06b433ce2d8243b2df0619
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-10-21 14:09:13 +0300

    Восстановление верного кода 1го урока

--- script/script.js
+++ script/script.js
@@ -1,17 +1,9 @@
-let money = 40000; // Доход за месяц
-let income = "40000"; //Доп доход, фриланс
-let addExpenses = "Кредит,Ремонт,Обучение"; // Доп расходы 
-let deposit = true; // Наличие депозита
-let mission = 1500000; // Какую сумму хочу накопить
-let period = 12; //Период
-let budgetDay = money/30; //Дневной бюджет
-
-console.log( "typeof(money): "+ typeof(money) );
-console.log( "income.length: "+ income.length );
-console.log( "typeof(deposit): "+ typeof(deposit));
-console.log( "Период " + period + " месяцев. " + 
-"Цель заработать "+ mission +" рублей" );
-addExpenses = addExpenses.toLowerCase();
-console.log("addExpenses as array: " + addExpenses.split("") );
-console.log( "budgetDay: " + budgetDay);
-console.log( "money % 30: " + money % 30 );
+// 1 Lesson
+let money;
+let income; 
+let addExpenses;
+let deposit;
+let mission; 
+let period;
+alert( "Ready, Steady, Go" );
+console.log( "Execution begin" );
