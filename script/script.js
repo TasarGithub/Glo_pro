61e6861d94c0f93dd8d50885509dc0565ec5ed01
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-10-22 23:11:53 +0300

    Up

--- script/script.js
+++ script/script.js
@@ -1,18 +1,6 @@
-// 4 lesson
-const money = 40000; // Доход за месяц
-const income = "40000"; //Доп доход, фриланс
-let addExpenses = "Кредит,Ремонт,Обучение"; // Доп расходы 
-const deposit = true; // Наличие депозита
-const mission = 1500000; // Какую сумму хочу накопить
-const period = 12; //Период
-const budgetDay = money/30; //Дневной бюджет
+// Lesson 04
+    function outputMessage(){
+        console.log('Hello world');
+    }
 
-console.log( "typeof(money): "+ typeof(money) );
-console.log( "income.length: "+ income.length );
-console.log( "typeof(deposit): "+ typeof(deposit));
-console.log( "Период " + period + " месяцев. " + 
-"Цель заработать "+ mission +" рублей" );
-addExpenses = addExpenses.toLowerCase();
-console.log("addExpenses as array: " + addExpenses.split(",") );
-console.log( "budgetDay: " + budgetDay);
-console.log( "money % 30: " + money % 30 );
+    outputMessage ();
