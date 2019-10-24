122d0cf9035dcd8d42a9bca09fc680d012e74089
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-10-24 13:07:21 +0300

    Переписать функцию start циклом do while

--- script/script.js
+++ script/script.js
@@ -5,19 +5,19 @@ const income = '40000', //Доп доход, фриланс
     mission = 1500000, // Какую сумму хочу накопить
     period = 12; //Период
 
-// после комментария Максима изменил на (money <=0), но тогда не учитывает вариант NaN, поэтому вернул назад, 
-let money = +prompt('Ваш месячный доход?',80000);
+let money; 
+//1) Переписать функцию start циклом do while
 let start = function(){
-    while  (!(money > 0)) {
-    //alert("Введите числовое значение, больше нуля");
-    money = +prompt('Ваш месячный доход?',80000);
-    } 
+    do {
+        money = +prompt('Ваш месячный доход?',80000);    
+    } while  (money <= 0 || isNaN(money) || money == null) ;
 }
-start()
+start();
 
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
     deposit = confirm ('Есть ли у вас депозит в банке?'),
     budgetDay = money/30; //Дневной бюджет
+
 // в 5 уроке showTypeof не требуется для вывода, поэтому комментирую
 //     let showTypeof = function(data){
 //     console.log(data, typeof(data));
@@ -42,8 +42,8 @@ let getExpensesMonth = function(){
                 
         if (i == 0) Expenses1 = prompt('Введите обязательную статью расходов.', 'Транспорт');
         if (i == 1) Expenses2 = prompt('Введите обязательную статью расходов.', 'Кредит');
-        
-        //Проверка корректности ввода расхода
+        //2) Добавить валидацию (проверку) на данные которые мы получаем на вопрос 
+        //'Во сколько это обойдется?’ в функции  getExpensesMonth
         do {
             ExpensesAmount = +prompt('Во сколько это обойдется? ', 7000);
         } while  (!(ExpensesAmount > 0));
@@ -53,7 +53,6 @@ let getExpensesMonth = function(){
     return sum;
 }
 
-
 function getAccumulatedMonth(money_par,income_par,getExpensesMonth_par){
     return  +money_par + (+income_par) - getExpensesMonth_par;
 }
@@ -62,13 +61,14 @@ let accumulatedMonth = getAccumulatedMonth(money,income,getExpensesMonth ());
 console.log('Накопления за период: ', accumulatedMonth);
 
 // budgetMonth = accumulatedMonth, поэтому заккоментировал.
-
 // let budgetMonth = (+money) + (+income) - ExpensesMonth;
-// console.log('budgetMonth = (+money) + (+income) - ExpensesMonth;: ', budgetMonth);
 
 // Поправить budgetDay учитывая бюджет на месяц, а не месячный доход. 
 budgetDay = Math.floor(accumulatedMonth / 30);
 
+
+//4) Если budgetDay отрицательное значение, 
+//   то вместо уровня дохода пусть выводится сообщение “Что то пошло не так”
 let getStatusIncome = function(){
     if (budgetDay <= 0){
         return ('Что-то пошло не так');
@@ -81,6 +81,9 @@ let getStatusIncome = function(){
     }
 }
 console.log('getStatusIncome() :',getStatusIncome());
+
+//3) Если getTargetMonth возвращает нам отрицательное значение, то вместо “Цель будет достигнута” 
+//необходимо выводить “Цель не будет достигнута”
 
 function getTargetMonth(mission_par,accumulatedMonth_par){
     return Math.floor(mission_par/accumulatedMonth_par);
