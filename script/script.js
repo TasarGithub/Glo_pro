139abaaa3ac83e10267db0a46b10a82ea3447aed
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-10-24 11:51:56 +0300

    убрал блоки двойного вычисления бюджета на месяц = accumulatedMonth, добавил внутр переменную, ExpensesAmount в getExpensesMonth

--- script/script.js
+++ script/script.js
@@ -6,11 +6,15 @@ const income = '40000', //Доп доход, фриланс
     period = 12; //Период
 
 // после комментария Максима изменил на (money <=0), но тогда не учитывает вариант NaN, поэтому вернул назад, 
-let money = +prompt('Ваш месячный доход?');
-while  (!(money > 0)) {
-   alert("Введите числовое значение, больше нуля");
-   money = parseInt(prompt('Ваш месячный доход?'));
-} 
+let money = +prompt('Ваш месячный доход?',80000);
+let start = function(){
+    while  (!(money > 0)) {
+    //alert("Введите числовое значение, больше нуля");
+    money = +prompt('Ваш месячный доход?',80000);
+    } 
+}
+start()
+
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
     deposit = confirm ('Есть ли у вас депозит в банке?'),
     budgetDay = money/30; //Дневной бюджет
@@ -23,29 +27,46 @@ showTypeof (money);
 showTypeof (income);
 showTypeof (deposit);
 
-let Expenses1 = prompt('Обязательный ежемесячный расход. Вариант1'),
-    ExpensesAmount1=  +prompt('Во сколько это обойдется? Вариант1', 5000);
 
-//Проверка корректности ввода расходов
-while  (!(ExpensesAmount1 > 0)) {
-    alert("Введит числовое значение, больше нуля");
-    ExpensesAmount1 = +prompt('Обязательные ежемесячные расходы, сумма. Введите числом');
-} 
+let Expenses1,
+    Expenses2;
+    
 
-let Expenses2 = prompt('Обязательный ежемесячный расход. Вариант2'),
-    ExpensesAmount2 = +prompt('Во сколько это обойдется? Вариант2', 7000);
+let getExpensesMonth = function(){
+    
+    let ExpensesAmount,
+        sum = 0;
+    
+    for (let i = 0; i < 2; i++){
+                
+        if (i == 0) Expenses1 = prompt('Введите обязательную статью расходов.', 'Транспорт');
+        if (i == 1) Expenses2 = prompt('Введите обязательную статью расходов.', 'Кредит');
 
-//Проверка корректности ввода расходов
-while  (!(ExpensesAmount2 > 0)) {
-    alert("Введит числовое значение, больше нуля");
-    ExpensesAmount2 = +prompt('Обязательные ежемесячные расходы, сумма. Введите числом');
-} 
+        // ExpensesAmount = +prompt('Во сколько это обойдется? ', 7000);
+        // //Проверка корректности ввода расхода
+        // while  (!(ExpensesAmount > 0)) {
+        //     ExpensesAmount = +prompt('Во сколько это обойдется? ', 7000);
+        // }
+        
+        //Проверка корректности ввода расхода
+        do {
+            ExpensesAmount = +prompt('Во сколько это обойдется? ', 7000);
+        } while  (!(ExpensesAmount > 0));
+        
+        sum+=ExpensesAmount;
+    }
+    return sum;
+}
 
-//  Вычислить доход за месяц, учитывая обязательные расходы, 
-let budgetMonth = (+money) + (+income) - ExpensesAmount1 - ExpensesAmount2;
+// // Так как в скрипте дважды используется вычисляемое getExpensesMonth зачение 
+// let ExpensesMonth = getExpensesMonth (); 
 
-// Поправить budgetDay учитывая бюджет на месяц, а не месячный доход. 
-budgetDay = Math.floor(budgetMonth / 30);
+// //  Вычислить доход за месяц, учитывая обязательные расходы, 
+// let budgetMonth = (+money) + (+income) - ExpensesMonth;
+// console.log('budgetMonth = (+money) + (+income) - ExpensesMonth;: ', budgetMonth);
+
+// // Поправить budgetDay учитывая бюджет на месяц, а не месячный доход. 
+// budgetDay = Math.floor(budgetMonth / 30);
 
 let getStatusIncome = function(){
     if (budgetDay <= 300) {
@@ -58,18 +79,18 @@ let getStatusIncome = function(){
 }
 console.log(getStatusIncome());
 
-function getExpensesMonth(exp1,exp2){
-    return +exp1 + (+exp2);
-    }
-
 function getAccumulatedMonth(money_par,income_par,getExpensesMonth_par){
     return  +money_par + (+income_par) - getExpensesMonth_par;
 }
 
-let accumulatedMonth = getAccumulatedMonth(money,income,getExpensesMonth(ExpensesAmount1, ExpensesAmount2));
+let accumulatedMonth = getAccumulatedMonth(money,income,getExpensesMonth());
 console.log('Накопления за период: ', accumulatedMonth);
 
 function getTargetMonth(mission_par,accumulatedMonth_par){
     return Math.floor(mission_par/accumulatedMonth_par);
 }
-console.log('Cрок достижения цели в месяцах', getTargetMonth (mission,accumulatedMonth));
+if (getTargetMonth (mission,accumulatedMonth) > 0){
+    console.log('Cрок достижения цели в месяцах', getTargetMonth (mission,accumulatedMonth));
+} else {
+    console.log('Цель не будет достигнута, расходы выше доходов');
+}  
