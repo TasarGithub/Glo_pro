e2c74a81f9f236a252154af07b5087730f50f926
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-10-31 06:43:13 +0300

    you-dont-know-js done

---
+++ you-dont-know-js/Script/script.js
@@ -0,0 +1,51 @@
+//lesson07
+'use strict';
+
+// Используя только файл скрипта (html руками не трогать) выполнить такие действия:
+
+// Восстановить порядок книг.
+let myBook =  document.querySelectorAll('.book'),
+    myBooks = document.querySelectorAll('.books');
+
+myBooks[0].appendChild(myBook[2]);
+myBooks[0].insertBefore(myBook[1],myBook[0]);
+myBooks[0].insertBefore(myBook[3],myBook[5]);
+
+// Заменить картинку заднего фона на другую из папки image
+let  myBody = document.querySelector('body');
+myBody.setAttribute('style','background-image: url(./image/you-dont-know-js.jpg)');
+
+// Исправить заголовок в книге 3( Получится - "Книга 3. this и Прототипы Объектов")
+
+let myText = myBook[4].innerHTML.replace('Пропопипы','Прототипы');
+myBook[4].innerHTML = myText;
+
+// Удалить рекламу со страницы
+let myAdv = document.querySelector('.adv');
+myAdv.remove();
+
+// Восстановить порядок глав во второй и пятой книге
+let ul2 = myBook[0].querySelectorAll('ul'),
+    li2 = myBook[0].querySelectorAll('li');
+
+ul2[0].insertBefore(li2[2],li2[9]);
+ul2[0].insertBefore(li2[7],li2[2]);
+ul2[0].insertBefore(li2[5],li2[7]);
+ul2[0].insertBefore(li2[4],li2[5]);
+
+let ul5 = myBook[5].querySelectorAll('ul'),
+    li5 = myBook[5].querySelectorAll('li');
+
+ul5[0].insertBefore(li5[5],li5[10]);
+ul5[0].insertBefore(li5[8],li5[10]);
+ul5[0].insertBefore(li5[2],li5[6]);
+ul5[0].insertBefore(li5[9],li5[3]);
+
+// в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
+
+let ul6 = myBook[2].querySelectorAll('ul'),
+    li6 = myBook[2].querySelectorAll('li');
+
+let newElem = document.createElement('li');
+newElem.textContent = 'Глава 8: За пределами ES6' 
+ul6[0].insertBefore(newElem,li6[9]);
