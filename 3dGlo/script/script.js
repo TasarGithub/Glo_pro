af003b58aa6ec74317e4b3a7d83294c2288147e6
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-11-20 23:24:47 +0300

    3dglo done

--- 3dGlo/script/script.js
+++ 3dGlo/script/script.js
@@ -2,41 +2,55 @@ window.addEventListener('DOMContentLoaded', function () {
   'use strict';
 
   // таймер
-  function countTimer(deadLine) {
+  let countTimer = (deadLine) => {
     //debugger;
     let timerdays= document.querySelector('#timer-days'),
         timerhours= document.querySelector('#timer-hours'),
         timerminutes = document.querySelector('#timer-minutes'),
         timerseconds = document.querySelector('#timer-seconds');
 
-    function getTimeRemanining() {
-    let dateStop = new Date(deadLine).getTime(),
+    let getTimeRemanining = () => {
+      let dateStop = new Date(deadLine).getTime(),
         dateNow = new Date().getTime(),
         timeRemaning = (dateStop -dateNow) / 1000,
         seconds = Math.floor(timeRemaning % 60),
         minutes = Math.floor((timeRemaning / 60) % 60),
         hours = Math.floor (timeRemaning / 60 / 60 % 24),
         days = Math.floor (timeRemaning / 60 / 60 / 24);
-        return {timeRemaning, days, hours, minutes, seconds};
+      return {timeRemaning, days, hours, minutes, seconds};
         
     }
-    function updateClock() {
+    let updateClock = () => {
       let timer = getTimeRemanining();
-
-      timerhours.textContent = timer.hours;
-      timerminutes.textContent = timer.minutes;
-      timerseconds.textContent = timer.seconds;
-      timerdays.textContent = timer.days;
-
-      if (timer.timeRemaning > 0) {
-        setTimeout(updateClock, 1000);
-      }
-
+     
+      timerhours.textContent = (timer.hours < 10) ? ('0' + timer.hours) : timer.hours;
+      timerminutes.textContent = (timer.minutes < 10) ? ('0' + timer.minutes) : timer.minutes;
+      timerseconds.textContent = (timer.seconds < 10) ? ('0' + timer.seconds) : timer.seconds; 
+      timerdays.textContent = (timer.days < 10) ? ('0' + timer.days) : timer.days;
+      
+      console.log('timer.timeRemaning: ', timer.timeRemaning);
     }
-    updateClock();
-
+    
+    let timer = getTimeRemanining();
+    if (timer.timeRemaning > 0) {
+      console.log('timer.timeRemaning: ', timer.timeRemaning);
+      console.log('timer.timeRemaning - 873700: ', (timer.timeRemaning - 873600)*1000);
+      let idTimer = setInterval(updateClock, 1000);
+      setTimeout( () => {
+        clearTimeout(idTimer);
+      }, ((timer.timeRemaning - 867000)*1000));
+    } else {
+      timerhours.textContent = '00';
+      timerminutes.textContent = '00';
+      timerseconds.textContent = '00'; 
+      timerdays.textContent = '00';
+    }
   }
+    
+
   //setInterval(countTimer , 1000, '1 december 2019');
   //console.log(timeRemaning);
-  countTimer('1 december 2019')
+  
+  countTimer('1 december 2019');
+
 });
