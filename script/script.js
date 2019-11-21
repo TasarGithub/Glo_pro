c4f1f70c2baa28e7bd886d54dbfdb0ad2de01d05
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-11-21 13:14:02 +0300

    let to const everywhwere

--- script/script.js
+++ script/script.js
@@ -9,10 +9,10 @@
 window.addEventListener('DOMContentLoaded', function () {
   'use strict';
 
-  let zeroPlus = (n) => (n < 10) ? '0' + n : n;
+  const zeroPlus = (n) => (n < 10) ? '0' + n : n;
 
-  let getTimeRemanining = (deadLine) => {
-    let dateStop = new Date(deadLine).getTime(),
+  const getTimeRemanining = (deadLine) => {
+    const dateStop = new Date(deadLine).getTime(),
       dateNow = new Date().getTime(),
       timeRemaning = (dateStop -dateNow) / 1000,
       date = new Date(),
@@ -21,11 +21,11 @@ window.addEventListener('DOMContentLoaded', function () {
       hours = date.getHours(),
       daysOfWeek = date.getDay(),
       daysRemainig = Math.floor (timeRemaning / 60 / 60 / 24);
-    return {timeRemaning, daysRemainig, hours, daysOfWeek, minutes, seconds};
+    return {daysRemainig, hours, daysOfWeek, minutes, seconds};
       
   }
 
-  let wd = (d) => {
+  const wd = (d) => {
     switch (d) {
       case 0: {
         return 'Воскресенье';
@@ -58,26 +58,25 @@ window.addEventListener('DOMContentLoaded', function () {
     }
   } 
 
-  let countTimer = (deadLine) => {
-    let GoodDay = document.createElement('p'),
-        today = document.createElement('p'),
-        timeNow = document.createElement('p'),
-        timeUntil = document.createElement('p');
+  const countTimer = (deadLine) => {
+    const GoodDay = document.createElement('p'),
+      today = document.createElement('p'),
+      timeNow = document.createElement('p'),
+      timeUntil = document.createElement('p'),
+      timer = getTimeRemanining(deadLine);
+      
+    GoodDay.textContent = (timer.hours > 6 && timer.hours < 12) ? 'Доброе утро' :
+                          ((timer.hours > 12 &&  timer.hours < 19 ) ? 'Добрый день' :
+                          ((timer.hours > 19 &&  timer.hours < 24 ) ? 'Добрый вечер' : 'Добрая ночь'));
+    today.textContent = wd(timer.daysOfWeek);
+    timeNow.textContent = `Текущее время : ${(timer.hours > 12) ? zeroPlus(timer.hours-12) :
+                          zeroPlus(timer.hours)}:${zeroPlus(timer.minutes)}:${zeroPlus(timer.seconds)} ${(timer.hours > 12) ? 'PM' : 'AM'}`;
+    timeUntil.textContent = `До нового года осталось ${timer.daysRemainig} дней`;
     
-      let timer = getTimeRemanining(deadLine);
-      
-      GoodDay.textContent = (timer.hours > 6 && timer.hours < 12) ? 'Доброе утро' :
-                            ((timer.hours > 12 &&  timer.hours < 19 ) ? 'Добрый день' :
-                            ((timer.hours > 19 &&  timer.hours < 24 ) ? 'Добрый вечер' : 'Добрая ночь'));
-      today.textContent = wd(timer.daysOfWeek);
-      timeNow.textContent = `Текущее время : ${(timer.hours > 12) ? zeroPlus(timer.hours-12) :
-                            zeroPlus(timer.hours)}:${zeroPlus(timer.minutes)}:${zeroPlus(timer.seconds)} ${(timer.hours > 12) ? 'PM' : 'AM'}`;
-      timeUntil.textContent = `До нового года осталось ${timer.daysRemainig} дней`;
-      
-      document.body.append(GoodDay);
-      document.body.append(today);
-      document.body.append(timeNow);
-      document.body.append(timeUntil);
+    document.body.append(GoodDay);
+    document.body.append(today);
+    document.body.append(timeNow);
+    document.body.append(timeUntil);
   }
 
   countTimer('1 january 2020');
