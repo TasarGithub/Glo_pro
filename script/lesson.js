0c0cebe125f27e591ce17cd0c16aa774533d3f69
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-11-21 15:43:34 +0300

    lesson.js  start other

--- script/lesson.js
+++ script/lesson.js
@@ -1,65 +1,11 @@
-'use strict';
-//lesson11.2
+const block = document.querySelector('.boxing');
 
-class CarWash {
-  constructor(brand, model = noCarBaseModel(), services = []){
-    this.brand = brand;
-    this.model = model;
-    this.washed = false;
-    this.services = services;
+console.dir(block);
 
-  }
-  washReady() {
-    this.washed = true;
-    CarWash.counter++;
-    this.report();
-  }
-  static noCarBaseModel(){
-     return 'none';
-  }
+const height = block.scrollHeight;
+const width = block.scrollWidth;
 
-  report() {
-    console.log(this.brand, this.model, this.washed);
-  }
-
-  get services(){
-  
-  }
-}
-CarWash.counter = 0;
-
-const car1 = new CarWash('mazda', 3, ['black tires', 'wax']);
-const car2 = new CarWash('BMW', 6);
-const car3 = new CarWash('Mers', A200);
-const car4 = new CarWash('Zaz');
-
-car1.washReady();
-car2.washReady();
-car3.washReady();
-car4.washReady();
-
-console.log('CarWash.counter: ', CarWash.counter);
-
-
-
-
-const car = {
-  model: '3',
-  year: 2006
-  brand: mazda
-  get fullTitle(){
-    return this.brand + ' ' + this.model;
-  }
-  set function(value) {
-    this.brand = value;
-  }
-};
-
-Object.defineProperty (mazda, 'fullTitle', {
-  get: function() {
-    return this.brand + ' ' + this.model;
-  },
-  set: function(val) {
-    this.brand = val
-  }
- })
+document.querySelector('.box_button').addEventListener('click',()=>{
+  block.style.height = `${block.scrollHeight}px`;
+  block.style.width = `${block.scrollWidth}px`;
+})
