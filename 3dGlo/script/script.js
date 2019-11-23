67c32fe405e57d2164ee8c96cb163c77ab069b94
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-11-23 14:17:07 +0300

    start

--- 3dGlo/script/script.js
+++ 3dGlo/script/script.js
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', function () {
         timerminutes = document.querySelector('#timer-minutes'),
         timerseconds = document.querySelector('#timer-seconds');
 
-    const getTimeRemanining = () => {
+    let getTimeRemanining = () => {
       const dateStop = new Date(deadLine).getTime(),
         dateNow = new Date().getTime(),
         timeRemaning = (dateStop -dateNow) / 1000,
@@ -28,7 +28,6 @@ window.addEventListener('DOMContentLoaded', function () {
       timerseconds.textContent = (timer.seconds < 10) ? ('0' + timer.seconds) : timer.seconds; 
       timerdays.textContent = (timer.days < 10) ? ('0' + timer.days) : timer.days;
       
-      console.log('timer.timeRemaning: ', timer.timeRemaning);
     }
     
     const timer = getTimeRemanining();
@@ -44,7 +43,84 @@ window.addEventListener('DOMContentLoaded', function () {
       timerdays.textContent = '00';
     }
   }
+
+  countTimer('1 december 2019');
+
+
+
+  //menu
+  const toggleMenu = () =>{
+    const btnMenu =  document.querySelector('.menu'),
+      menu =  document.querySelector('menu'),
+      closeBtn = document.querySelector('.close-btn'),
+      menuItems = document.querySelectorAll('ul>li');
+       //закрытие/открытие меню 
+      const handlerMenu = () => {
+        menu.classList.toggle('active-menu')
+      }
     
-countTimer('1 december 2019');
+  btnMenu.addEventListener('click', handlerMenu);
+    
+  closeBtn.addEventListener('click', handlerMenu);
+  
+  // закрытие при нажатии на какой-либо пункт меню
+  menuItems.forEach( (elem) => elem.addEventListener('click',handlerMenu));
+  }
+  toggleMenu();
+
+  //popup
+  const togglePopUp = () => {
+    const popup = document.querySelector('.popup'),
+          popupContent = document.querySelector('.popup-content'),
+    // кнопки запуска модальных окон
+      popupBtn = document.querySelectorAll('.popup-btn'),
+      popUpClose = document.querySelector('.popup-close');
+    popupBtn.forEach((elem) => {
+      elem.addEventListener('click', () => {
+        popup.style.display = 'block';
+        popupContent.style.position = 'relative';
+       flyInterval = requestAnimationFrame(flyAnimate);
+      });
+    });
+
+    popUpClose.addEventListener('click', () => {
+      popup.style.display = 'none';
+      popupContent.style.position = 'fixed';
+      cancelAnimationFrame(flyInterval);
+    });
+
+    //anime
+    let flyInterval,
+    count = 1;
+
+    let flyAnimate = () => {
+      //debugger;
+      flyInterval = requestAnimationFrame(flyAnimate);
+      count++;
+
+      //document.documentElement.clientWidth
+      //console.log('document.documentElement.clientWidth: ', document.documentElement.clientWidth);
+
+      if(screen.width > 768){
+        if(count < (document.documentElement.clientWidth - popupContent.offsetWidth)&&
+          (count < (document.documentElement.clientHeight - popupContent.offsetHeight))){
+            popupContent.style.left = count + 'px';
+            popupContent.style.top = count + 'px';
+        } else if (count < (document.documentElement.clientHeight - popupContent.offsetHeight)){
+          
+          popupContent.style.top = count + 'px';
+        } else if (count < (document.documentElement.clientWidth - popupContent.offsetWidth)) {
+          popupContent.style.left = count + 'px';
+        } else {
+          cancelAnimationFrame(flyInterval);
+        }
+      } else {
+        cancelAnimationFrame(flyInterval);
+      }
+    };
+    
+  };
+
+  togglePopUp();
 
 });
