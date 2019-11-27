a4e1daf02e2e08c3fe5d35639f3f3d4239cfee94
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-11-27 23:37:18 +0300

    lesson  les17

--- script/lesson.js
+++ script/lesson.js
@@ -3,6 +3,25 @@
 
 //
 document.addEventListener('DOMContentLoaded', () => {
+  const select = document.getElementById('cars'),
+    output = document.getElementById('output');
+  select.addEventListener('change', (event) =>{
+    const request = new XMLHttpRequest();
+
+    request.open('GET', './cars.json');
+  
+    request.setRequestHeader('Content-type', 'application/json');
+  
+    request.send();
+  
+    request.addEventListener('loadend',(event) => {
+      if (request.readyState === 4 && request.status === 200) {
+
+        const data = JSON.parse(request.responseText);
+        
+      }
+    });
+  });
 
 
 
