3e072c04adccb21e6c0edb22e1d6e90af998d350
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-11-27 18:03:25 +0300

    lesson17 start

--- script/lesson.js
+++ script/lesson.js
@@ -1,24 +1,4 @@
 'use strict';
-//lesson14
+//lesson17
 
-const buttons = document.querySelectorAll('.button');
-content = document.querySelector('.content');
-
-wrapButtons = document.querySelector
-
-const changeText = (element) => {
-  content.textContent = element.textContent;
-};
-
-for (let i = 0, btnlnt = buttons.length; i < btnlnt; i++) {
-  buttons[i].addEventListener('click', () => changeText(buttons[i]));
-}
-
-function addButton() {
-  const newButton = buttons[0].cloneNode();
-  let textButton = buttons.length + 1;
-  if (textButton < 10) {
-    textButton = `0${textButton}`;
-  }
-  wrapButtons.appendChild(newButton);
-}
+//dsd
