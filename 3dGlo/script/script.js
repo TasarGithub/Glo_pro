9d98cc7ca707fd5b4f90360a0c1f4b3d896b7728
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-11-30 20:04:51 +0300

    l14 add Plural Forms hw done

--- 3dGlo/script/script.js
+++ 3dGlo/script/script.js
@@ -3,6 +3,10 @@ window.addEventListener('DOMContentLoaded', function () {
 
   // таймер
   const countTimer = (deadLine) => {
+
+
+
+
     //debugger;
     const timerdays= document.querySelector('#timer-days'),
         timerhours= document.querySelector('#timer-hours'),
@@ -19,16 +23,30 @@ window.addEventListener('DOMContentLoaded', function () {
         days = Math.floor (timeRemaning / 60 / 60 / 24);
       return {timeRemaning, days, hours, minutes, seconds};
         
-    }
+    };
+    
+    const declOfNum = (n, titles)  => {
+      return titles[(n % 10 === 1 && n % 100 !== 11) ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2]
+    };
+    console.log(declOfNum(1,['день', 'дня', 'дней']));
+    console.log(declOfNum(2,['день', 'дня', 'дней']));
+    console.log(declOfNum(3,['день', 'дня', 'дней']));
+    console.log(declOfNum(4,['день', 'дня', 'дней']));
+    console.log(declOfNum(5,['день', 'дня', 'дней']));
+
     const updateClock = () => {
       const timer = getTimeRemanining();
      
       timerhours.textContent = (timer.hours < 10) ? ('0' + timer.hours) : timer.hours;
       timerminutes.textContent = (timer.minutes < 10) ? ('0' + timer.minutes) : timer.minutes;
       timerseconds.textContent = (timer.seconds < 10) ? ('0' + timer.seconds) : timer.seconds; 
-      timerdays.textContent = (timer.days < 10) ? ('0' + timer.days) : timer.days;
-      
-    }
+      if (timer.days === 0) {
+        timerdays.remove();
+      } else {
+        timerdays.textContent = `${((timer.days < 10) ? ('0' + timer.days) : timer.days)}
+                                 ${declOfNum(timer.days, ['день', 'дня', 'дней'])} : `;
+      }
+    };
     
     const timer = getTimeRemanining();
     if (timer.timeRemaning > 0) {
@@ -51,7 +69,7 @@ window.addEventListener('DOMContentLoaded', function () {
     const btnMenu =  document.querySelector('.menu'),
       menu =  document.querySelector('menu'),
        //закрытие/открытие меню 
-      const handlerMenu = () => {
+      handlerMenu = () => {
         menu.classList.toggle('active-menu');
       };
     
